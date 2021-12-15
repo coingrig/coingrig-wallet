3c57d52083e78ba411b2b4cf3cdd3e7e20e8817d
Razvan Rujoiu <rjrazvan@icloud.com>
2021-12-15 22:41:58 +0200

    Implemented AppSwitcher Obscuring on iOS

--- ios/Coingrig/AppDelegate.m
+++ ios/Coingrig/AppDelegate.m
@@ -52,6 +52,55 @@ static void InitializeFlipper(UIApplication *application) {
   return YES;
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application {
+  UIImage* blurred = [self blurredImageWithImage: [self createScreenShotOfCurrentContext]];
+  _appSwitcherView = [[UIImageView alloc] initWithFrame: [[self window] bounds]]; //just example change the frame as per your need
+  _appSwitcherView.image = blurred;
+  [[self window ] addSubview: [self appSwitcherView]];
+  
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [_appSwitcherView removeFromSuperview];
+}
+
+- (UIImage*)createScreenShotOfCurrentContext {
+  CGRect rect = [self.window bounds];
+  UIGraphicsBeginImageContextWithOptions(rect.size,YES,0.0f);
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  [self.window.layer renderInContext:context];
+  UIImage *capturedImage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return  capturedImage;
+}
+
+
+- (UIImage *)blurredImageWithImage:(UIImage *)sourceImage{
+
+    //  Create our blurred image
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CIImage *inputImage = [CIImage imageWithCGImage:sourceImage.CGImage];
+
+    //  Setting up Gaussian Blur
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [filter setValue:inputImage forKey:kCIInputImageKey];
+    [filter setValue:[NSNumber numberWithFloat:20.0f] forKey:@"inputRadius"];
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];
+
+    /*  CIGaussianBlur has a tendency to shrink the image a little, this ensures it matches
+     *  up exactly to the bounds of our original image */
+    CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
+
+    UIImage *retVal = [UIImage imageWithCGImage:cgImage];
+
+    if (cgImage) {
+        CGImageRelease(cgImage);
+    }
+
+    return retVal;
+}
+
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
